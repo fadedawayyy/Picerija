bc78fe9b4c1adb632e1d6753eff23b715925c0a0
fadedawayyy <gergot.gg@gmail.com>
2026-01-19 18:34:49 +0200

    Pievienotas ikonas un logo

--- src/lvtPicerija/Design.java
+++ src/lvtPicerija/Design.java
@@ -84,4 +84,109 @@ public class Design {
 
    
 
+    public static void drawHeader(Graphics g, int w, int h) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2.setColor(COLOR_BG);
+        g2.fillRect(0, 0, w, h);
+
+        int iconSize = 80;
+        int gap = 20;
+        
+        g2.setFont(FONT_LOGO);
+        FontMetrics fm = g2.getFontMetrics();
+        int textW = fm.stringWidth("LVT PIZZA");
+        int totalW = iconSize + gap + textW;
+        
+        int startX = (w - totalW) / 2;
+        int centerY = h / 2;
+        int iconY = centerY - (iconSize / 2);
+
+        
+        g2.setColor(LOGO_DARK);
+        g2.fillOval(startX, iconY, iconSize, iconSize);
+        g2.setColor(PIZZA_CRUST);
+        g2.fillOval(startX + 4, iconY + 4, iconSize - 8, iconSize - 8);
+        g2.setColor(PIZZA_RED);
+        g2.fillOval(startX + 8, iconY + 8, iconSize - 16, iconSize - 16);
+        g2.setColor(PIZZA_CHEESE);
+        g2.fillOval(startX + 15, iconY + 15, iconSize - 30, iconSize - 30); 
+        g2.setColor(new Color(200, 60, 60));
+        g2.fillOval(startX + 25, iconY + 25, 12, 12);
+        g2.fillOval(startX + 45, iconY + 20, 12, 12);
+        g2.fillOval(startX + 30, iconY + 45, 12, 12);
+        g2.fillOval(startX + 50, iconY + 40, 12, 12);
+
+       
+        int textX = startX + iconSize + gap;
+        int textBaseline = centerY + (fm.getAscent() / 2) - 5;
+
+        g2.setColor(LOGO_DARK);
+        g2.fillRect(textX, centerY - 25, textW + 10, 4);
+
+        g2.drawString("LVT", textX, textBaseline);
+        int widthLVT = fm.stringWidth("LVT ");
+        g2.setColor(LOGO_ORANGE);
+        g2.drawString("PIZZA", textX + widthLVT, textBaseline);
+
+        int yLineBottom = centerY + 22;
+        g2.setColor(LOGO_DARK);
+        g2.fillRect(textX, yLineBottom, widthLVT - 5, 4);
+        g2.setColor(LOGO_ORANGE);
+        g2.fillRect(textX + widthLVT, yLineBottom, fm.stringWidth("PIZZA") + 10, 4);
+
+        g2.dispose();
     }
+
+    public static void drawProductIcon(Graphics g, ProductType type) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        if (type == ProductType.PIZZA) {
+            g2.setColor(new Color(230, 150, 50)); 
+            g2.fillOval(5, 5, 110, 110);
+            g2.setColor(new Color(255, 200, 80));
+            g2.fillOval(10, 10, 100, 100);
+            g2.setColor(new Color(200, 50, 50));
+            g2.fillOval(30, 30, 20, 20);
+            g2.fillOval(70, 40, 20, 20);
+            g2.fillOval(45, 80, 20, 20);
+        } else if (type == ProductType.DRINK) {
+            g2.setColor(new Color(220, 220, 220));
+            g2.fillOval(30, 100, 60, 15);
+            g2.setColor(new Color(50, 150, 250));
+            g2.fillRoundRect(35, 40, 50, 75, 10, 10);
+            g2.fillRect(48, 15, 24, 30);
+            g2.setColor(new Color(200, 50, 50));
+            g2.fillRect(46, 10, 28, 8);
+            g2.setColor(new Color(200, 0, 0)); 
+            g2.fillRect(35, 65, 50, 20);
+        } else {
+            g2.setColor(new Color(255, 215, 0)); 
+            g2.fillRoundRect(55, 20, 7, 70, 3, 3);
+            AffineTransform old = g2.getTransform();
+            drawFry(g2, old, -10, 50, 50, 45, 25, 7, 60);
+            drawFry(g2, old, 10, 70, 50, 65, 25, 7, 60);
+            drawFry(g2, old, -20, 40, 60, 35, 35, 7, 50);
+            drawFry(g2, old, 20, 80, 60, 75, 35, 7, 50);
+            g2.setColor(new Color(220, 20, 20)); 
+            Path2D box = new Path2D.Double();
+            box.moveTo(35, 110);
+            box.lineTo(85, 110);
+            box.lineTo(95, 60);
+            box.lineTo(25, 60);
+            box.closePath();
+            g2.fill(box);
+            g2.setColor(new Color(180, 0, 0));
+            g2.setStroke(new BasicStroke(1));
+            g2.draw(box);
+        }
+    }
+
+    private static void drawFry(Graphics2D g2, AffineTransform old, double rot, int anchorX, int anchorY, int x, int y, int w, int h) {
+        g2.rotate(Math.toRadians(rot), anchorX, anchorY);
+        g2.fillRoundRect(x, y, w, h, 3, 3);
+        g2.setTransform(old);
+    }
+}
